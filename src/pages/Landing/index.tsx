95e118c697e75feb0b43158a8778a270c9e509a8
Marcio Souza Filho <marciorasf@gmail.com>
2020-08-06 00:56:56 -0300

    add server and client projects

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Landing() {
+  return (
+    <h1>
+      Landing Page
+    </h1>
+  )
+}
+
+export default Landing
---
+++ src/routes.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { BrowserRouter, Route } from 'react-router-dom'
+import Landing from "./pages/Landing"
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Route path="/" component={Landing} />
+    </BrowserRouter>
+  )
+}
+
+export default Routes
