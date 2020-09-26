bcbc21ff626425d9f78d21ae5676603b04fa23f2
marciorasf <marciorasf@gmail.com>
2020-09-26 13:18:21 -0300

    remove redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Provider as ReduxProvider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 
 import "./assets/styles/reset.css";
@@ -8,28 +7,25 @@ import "react-toastify/dist/ReactToastify.css";
 import "./assets/styles/toast.css";
 import { Container } from "./assets/styles/global";
 import Routes from "./routes";
-import store from "./store";
 
 function App() {
   return (
-    <ReduxProvider store={store}>
-      <Container>
-        <ToastContainer
-          limit={1}
-          position="bottom-center"
-          autoClose={3000}
-          closeButton={false}
-          hideProgressBar={true}
-          pauseOnHover={false}
-          pauseOnFocusLoss={true}
-          closeOnClick={true}
-          draggable={true}
-          draggablePercent={70}
-          newestOnTop={true}
-        />
-        <Routes />
-      </Container>
-    </ReduxProvider>
+    <Container>
+      <ToastContainer
+        limit={1}
+        position="bottom-center"
+        autoClose={3000}
+        closeButton={false}
+        hideProgressBar={true}
+        pauseOnHover={false}
+        pauseOnFocusLoss={true}
+        closeOnClick={true}
+        draggable={true}
+        draggablePercent={70}
+        newestOnTop={true}
+      />
+      <Routes />
+    </Container>
   );
 }
 
--- src/store/user/action.ts
+++
@@ -1,8 +0,0 @@
-import { UserState, SET_USER, UserActionTypes } from "./types";
-
-export function setUser(user: UserState): UserActionTypes {
-  return {
-    type: SET_USER,
-    payload: user,
-  };
-}
