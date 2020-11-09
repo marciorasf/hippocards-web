857275c0152ed3373d0b4c3900127176ee45bdc5
marciorasf <marciorasf@gmail.com>
2020-11-09 19:45:28 -0300

    add route and page to change password

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import ChangePassword from "../pages/ChangePassword";
 import Landing from "../pages/Landing";
 
 function PublicRoutes() {
   return (
     <Switch>
       <Route path="/landing" exact component={Landing} />
+      <Route path="/change-password/:token" exact component={ChangePassword} />
     </Switch>
   );
 }
