17cc9c88cc9ca5a7b0d7edb0d5b9b438460040cd
marciorasf <marciorasf@gmail.com>
2021-03-30 19:04:35 -0300

    improve code

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,42 +5,55 @@ import { useHistory } from "react-router-dom"
 import { Button, Container, Grid } from "@material-ui/core"
 
 import InputField from "../../components/InputField"
+import Spacing from "../../components/Spacing"
 import apiService from "../../services/api"
 import errorService from "../../services/error"
 import { useUserStore } from "../../store/user"
+
+type LoginData = {
+  email: string
+  password: string
+}
 
 const Login: React.FC = () => {
   const userStore = useUserStore()
   const history = useHistory()
 
+  async function handleLogin(loginData: LoginData) {
+    try {
+      const { data } = await apiService.post("/login", loginData)
+
+      userStore.setUser({
+        email: data.email,
+      })
+
+      history.push("/categories")
+    } catch (err) {
+      errorService.handle(err.response)
+      const { message } = err.response.data
+      return message
+    }
+  }
+
   return (
     <Container>
+      <Spacing orientation="horizontal" size={4} />
+
       <Formik
         initialValues={{ email: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
-          try {
-            const { data } = await apiService.post("/login", values)
+          const message = await handleLogin(values)
 
-            userStore.setUser({
-              email: data.email,
+          if (message === "user_not_found") {
+            return setErrors({
+              email: "Email not found",
             })
+          }
 
-            history.push("/categories")
-          } catch (err) {
-            errorService.handle(err.response)
-            const { message } = err.response.data
-
-            if (message === "user_not_found") {
-              return setErrors({
-                email: "Email not found",
-              })
-            }
-
-            if (message === "wrong_password") {
-              return setErrors({
-                password: "Wrong password",
-              })
-            }
+          if (message === "wrong_password") {
+            return setErrors({
+              password: "Wrong password",
+            })
           }
         }}
       >
