4698902a20df54f7d6b811743a698796ee0f37ae
marciorasf <marciorasf@gmail.com>
2021-03-30 19:05:39 -0300

    move type to page file

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -1,35 +1,39 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 
-import { Container } from "@material-ui/core";
+import { Container } from "@material-ui/core"
 
-import useDidMount from "../../hooks/useDidMount";
-import { Category } from "../../interfaces/category";
-import apiService from "../../services/api";
-import errorService from "../../services/error";
+import useDidMount from "../../hooks/useDidMount"
+import apiService from "../../services/api"
+import errorService from "../../services/error"
+
+type Category = {
+  id: number
+  name: string
+}
 
 async function getCategories() {
   try {
-    const { data } = await apiService.get("/categories");
-    return data.categories as Category[];
+    const { data } = await apiService.get("/categories")
+    return data.categories as Category[]
   } catch (err) {
-    errorService.handle(err);
-    return [];
+    errorService.handle(err)
+    return []
   }
 }
 
 const Categories: React.FC = () => {
-  const [userCategories, setUserCategories] = useState<Category[]>([]);
+  const [userCategories, setUserCategories] = useState<Category[]>([])
 
   async function getAndUpdateCategories() {
-    const categories = await getCategories();
-    setUserCategories(categories);
+    const categories = await getCategories()
+    setUserCategories(categories)
   }
 
   useDidMount(() => {
-    getAndUpdateCategories();
-  });
+    getAndUpdateCategories()
+  })
 
-  return <Container>Categories</Container>;
-};
+  return <Container>Categories</Container>
+}
 
-export default Categories;
+export default Categories
