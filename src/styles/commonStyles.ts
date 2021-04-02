d4d0258f65a2ddb218c3fe984b942e790f7809f5
marciorasf <marciorasf@gmail.com>
2021-04-02 20:56:14 -0300

    add common styles file

---
+++ src/styles/commonStyles.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles(() => ({
+  fullHeight: {
+    height: "100%",
+  },
+  fullWidth: {
+    width: "100%",
+  },
+}))
+
+export default useStyles
