f447aad08e459b2b073b3ef795f244491dd61747
marciorasf <marciorasf@gmail.com>
2021-04-02 03:03:53 -0300

    add items to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
 
-import { Drawer, makeStyles } from "@material-ui/core"
+import useDidMount from "@hooks/useDidMount"
+import { Category } from "@interfaces/category"
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Collapse,
+  makeStyles,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@material-ui/core"
+import {
+  InfoOutlined as AboutIcon,
+  FolderOutlined as CategoryIcon,
+  ExpandLess as ExpandLessIcon,
+  ExpandMore as ExpandMoreIcon,
+} from "@material-ui/icons"
+import categoryService from "@services/category"
+import errorService from "@services/error"
 
 const useStyles = makeStyles((theme) => ({
-  menuButton: {
-    marginRight: theme.spacing(2),
+  list: {
+    width: 250,
   },
-  title: {
-    flexGrow: 1,
+  nested: {
+    paddingLeft: theme.spacing(8),
   },
 }))
 
@@ -17,11 +38,62 @@ type SidebarProps = {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ open, onClose }) => {
+  const [categories, setCategories] = useState<Category[]>([])
+  const [openCategories, setOpenCategories] = useState(true)
   const classes = useStyles()
+
+  async function getAndUpdateCategories() {
+    try {
+      const cat = await categoryService.retrieveAll()
+      setCategories(cat)
+    } catch (err) {
+      errorService.handle(err)
+    }
+  }
+
+  function handleToggleOpenCategories() {
+    setOpenCategories(!openCategories)
+  }
+
+  useDidMount(() => {
+    getAndUpdateCategories()
+  })
 
   return (
     <Drawer open={open} onClose={onClose}>
-      <div>Ola</div>
+      <List className={classes.list}>
+        <ListItem component={Link} to="/about" button>
+          <ListItemIcon>
+            <AboutIcon />
+          </ListItemIcon>
+
+          <ListItemText primary="about" />
+        </ListItem>
+
+        <ListItem component={Link} to="/categories" button>
+          <ListItemIcon>
+            <CategoryIcon />
+          </ListItemIcon>
+
+          <ListItemText primary="categories" />
+
+          <ListItemSecondaryAction>
+            <IconButton onClick={handleToggleOpenCategories}>
+              {openCategories ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+
+        <Collapse in={openCategories} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            {categories.map((category) => (
+              <ListItem key={category.id} button className={classes.nested}>
+                <ListItemText primary={category.name} />
+              </ListItem>
+            ))}
+          </List>
+        </Collapse>
+      </List>
     </Drawer>
   )
 }
