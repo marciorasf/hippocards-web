524f0bfb0793de6e5bad9913d3012479f2ad9126
marciorasf <marciorasf@gmail.com>
2021-04-02 23:32:10 -0300

    rename method to help intellisense

--- src/styles/commonStyles.ts
+++ src/styles/commonStyles.ts
@@ -1,6 +1,6 @@
 import { makeStyles } from "@material-ui/core"
 
-const useStyles = makeStyles(() => ({
+const useCommonStyles = makeStyles(() => ({
   fullHeight: {
     height: "100%",
   },
@@ -9,4 +9,4 @@ const useStyles = makeStyles(() => ({
   },
 }))
 
-export default useStyles
+export default useCommonStyles
