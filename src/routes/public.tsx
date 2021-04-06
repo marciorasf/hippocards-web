5f7e2da285f9b72110e155fc9df7d347770dc7ab
marciorasf <marciorasf@gmail.com>
2021-04-06 19:41:10 -0300

    add about page

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Route } from "react-router-dom"
 
+import About from "@pages/About"
 import Landing from "@pages/Landing"
 import Login from "@pages/Login"
 import Register from "@pages/Register"
@@ -9,6 +10,7 @@ const publicRoutes = [
   <Route exact key="landing" path="/" component={Landing} />,
   <Route exact key="login" path="/login" component={Login} />,
   <Route exact key="register" path="/register" component={Register} />,
+  <Route exact key="about" path="/about" component={About} />,
 ]
 
 export default publicRoutes
