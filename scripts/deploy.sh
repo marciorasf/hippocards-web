fe34aa06bb2875b46ed941260f25c64a8d738307
marciorasf <marciorasf@gmail.com>
2021-04-06 19:51:11 -0300

    remove obsolete files

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo heroku login 
-sudo heroku container:login
-sudo heroku container:push web
-sudo heroku container:release web
--- scripts/docker-build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sudo docker build --force-rm -t marciorasf/flashcards-web .
