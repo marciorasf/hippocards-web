fee4562ea086208c056f6af0e616d196ff92ad6c
marciorasf <marciorasf@gmail.com>
2021-04-20 19:56:11 -0300

    remove old files

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { Route } from "react-router-dom"
 
-import About from "@pages/About"
 import Landing from "@pages/Landing"
 import LogIn from "@pages/LogIn"
 import SignUp from "@pages/SignUp"
@@ -10,7 +9,6 @@ const publicRoutes = [
   <Route exact key="landing" path="/" component={Landing} />,
   <Route exact key="login" path="/login" component={LogIn} />,
   <Route exact key="signup" path="/signup" component={SignUp} />,
-  <Route exact key="about" path="/about" component={About} />,
 ]
 
 export default publicRoutes
